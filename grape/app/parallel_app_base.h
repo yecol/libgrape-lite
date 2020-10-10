70280df287c23a4120a4c25ad1b79b3e418e12e9
guanyi.gl <guanyi.gl@alibaba-inc.com>
2020-10-10 17:34:22 +0800

    remove ctx_data_t

--- grape/app/parallel_app_base.h
+++ grape/app/parallel_app_base.h
@@ -82,7 +82,6 @@ class ParallelAppBase {
  public:                                                          \
   using fragment_t = FRAG_T;                                      \
   using context_t = CONTEXT_T;                                    \
-  using ctx_data_t = typename context_t::data_t;                  \
   using message_manager_t = grape::ParallelMessageManager;        \
   using worker_t = grape::ParallelWorker<APP_T>;                  \
   virtual ~APP_T() {}                                             \
